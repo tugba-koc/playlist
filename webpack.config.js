c1faff3ab852e21714f3313bcd2638dcfbba019f
tugba-koc <koctug@itu.edu.tr>
2022-01-22 13:01:58 +0300

    create bundle html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    entry : './src/main.js',
-    output : {
-        path : __dirname + '/dist',
-        filename : 'bundle.js'
-    },
-}
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
