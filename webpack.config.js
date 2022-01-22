ea882dd995f8688927cd7c2d68fb625a70a301ca
tugba-koc <koctug@itu.edu.tr>
2022-01-22 14:00:11 +0300

    install css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry : './src/main.js',
-    output : {
-        path : __dirname + '/dist',
-        filename : 'bundle.js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template : "./src/index.html"
-    })],
-    mode: "development"
-}
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+};
