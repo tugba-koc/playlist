ca9d0573736e9e090fa502338a23dfbc97447b6f
tugba-koc <koctug@itu.edu.tr>
2022-01-23 18:47:00 +0300

    add a list-item function

--- src/controllers/landing.controller.js
+++ src/controllers/landing.controller.js
@@ -41,5 +41,82 @@ export default () => {
     overlay.classList.remove("active");
   }
 
+  // add item to the list
+
+  let values = [];
+  let list = divElement.querySelector("#list");
+
+  const saveButton = divElement.querySelector(".save");
+  const cancelButton = divElement.querySelector(".cancel");
+  let inputs = divElement.querySelectorAll("input");
+  let count = 0;
+
+  cancelButton.addEventListener("click", (event) => {
+    closeModal(modal);
+
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  });
+
+  let listItemStructure = "";
+  function addValues(values) {
+    listItemStructure += `
+    <li class="list-item">
+        
+          <p class="list-item-content-text-title">
+          ${values[0]}
+          </p>
+          <p class="list-item-content-text-description">
+          ${values[1]}
+          </p>
+          <p class="list-item-content-text-weight">
+          weight : ${values[3]}
+          </p>
+        
+        <div class="list-item-content-buttons">
+          <button class="list-item-content-buttons-edit">
+            <i class="fas fa-pen"></i>
+            edit
+          </button>
+          <button class="list-item-content-buttons-delete">
+            <i class="fas fa-trash"></i>
+            delete
+          </button>
+        </div>
+     
+    </li>
+  `;
+  }
+
+  saveButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    count++;
+    let id = new Date().getTime();
+
+    inputs.forEach((input) => {
+      values.push(input.value);
+    });
+
+    if (values[0] == "" || values[1] == "") {
+      values = values.slice(3);
+      return false;
+    }
+
+    values.push(id);
+
+    values.unshift(count);
+
+    addValues(values);
+
+    console.log(values);
+    list.innerHTML = listItemStructure;
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+    closeModal(modal);
+    values = [];
+  });
+
   return divElement;
 };
