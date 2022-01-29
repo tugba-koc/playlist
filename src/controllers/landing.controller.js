12feb9c8408417a240194ae656e27a4e59a129c6
tugba-koc <koctug@itu.edu.tr>
2022-01-29 21:05:18 +0300

    add and edit item functions

--- src/controllers/landing.controller.js
+++ src/controllers/landing.controller.js
@@ -44,14 +44,12 @@ export default () => {
   // add item to the list
 
   let values = [];
-  let listItems = [];
+  let listItemsArray = [];
   let list = divElement.querySelector("#list");
 
   const saveButton = divElement.querySelector(".save");
   const cancelButton = divElement.querySelector(".cancel");
   let inputs = divElement.querySelectorAll("input");
-
-  let listItemStructure = "";
 
   cancelButton.addEventListener("click", () => {
     closeModal(modal);
@@ -63,12 +61,15 @@ export default () => {
   saveButton.addEventListener("click", (event) => {
     event.preventDefault();
 
+    // create unique id for the item
     let id = new Date().getTime();
 
+    // get the values from the input fields
     inputs.forEach((input) => {
       values.push(input.value);
     });
 
+    // prevent empty values
     if (values[0] == "" || values[1] == "") {
       values = values.slice(3);
       return false;
@@ -76,63 +77,114 @@ export default () => {
 
     values.push(id);
 
-    values.unshift(listItems.length + 1);
+    values.unshift(listItemsArray.length + 1);
 
-    list.innerHTML += `
-    <li class="list-item">
-        
-          <p class="list-item-content-text-title">
-          ${values[0]}
-          </p>
-          <p class="list-item-content-text-description">
-          ${values[1]}
-          </p>
-          <p class="list-item-content-text-weight">
-          weight : ${values[3]}
-          </p>
-        
-        <div class="list-item-content-buttons">
-          <button class="list-item-content-buttons-edit">
-          <i class="fa fa-pencil-square-o fa-lg" ></i>
-          </button>
-          <button id=${values[4]} class="list-item-content-buttons-delete">
-          
-          <i class="fa fa-trash fa-lg" ></i>
-          </button>
-        </div>
-     
-    </li>
-  `;
+    // create a new list item
+    let listItem = document.createElement("li");
+    listItem.classList.add("list-item");
 
-    listItems.push(values);
+    let pTitle = document.createElement("p");
+    pTitle.classList.add("list-item-content-text-title");
+    pTitle.innerHTML = values[0];
 
-    let counts = divElement.querySelectorAll(".list-item-content-text-title");
+    let pDescription = document.createElement("p");
+    pDescription.classList.add("list-item-content-text-description");
+    pDescription.innerHTML = values[1];
 
-    let deleteButtons = divElement.querySelectorAll(
-      ".list-item-content-buttons-delete"
-    );
+    let pWeight = document.createElement("p");
+    pWeight.classList.add("list-item-content-text-weight");
+    pWeight.innerHTML = "weight : " + values[3];
 
-    let editButtons = divElement.querySelectorAll(
-      ".list-item-content-buttons-edit"
-    );
+    let div = document.createElement("div");
+    div.classList.add("list-item-content-buttons");
 
-    deleteButtons.forEach((button) => {
-      button.addEventListener("click", (event) => {
-        let item = event.target.closest(".list-item");
-        item.remove();
-        counts.forEach((count) => (count.innerHTML = count.innerHTML - 1));
-        listItems = listItems.filter((item) => item[4] != event.target.id);
-      });
-    });
+    let buttonEdit = document.createElement("button");
+    buttonEdit.classList.add("list-item-content-buttons-edit");
+    buttonEdit.id = values[4];
+    buttonEdit.onclick = function () {
+      editItem(this.id, listItem);
+    };
 
+    let iEdit = document.createElement("i");
+    iEdit.classList.add("fa", "fa-pencil-square-o", "fa-lg");
 
+    let buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("list-item-content-buttons-delete");
+    buttonDelete.id = values[4];
+    buttonDelete.onclick = function () {
+      deleteItem(this.id, listItem);
+    };
+
+    let iDelete = document.createElement("i");
+    iDelete.classList.add("fa", "fa-trash", "fa-lg");
+
+    // add the new list item to the list
+    list.appendChild(listItem);
+
+    // add the content to the list item
+    listItem.appendChild(pTitle);
+    listItem.appendChild(pDescription);
+    listItem.appendChild(pWeight);
+    listItem.appendChild(div);
+    div.appendChild(buttonEdit);
+    div.appendChild(buttonDelete);
+    buttonEdit.appendChild(iEdit);
+    buttonDelete.appendChild(iDelete);
+
+    listItemsArray.push(values);
+
+    // clear the input fields
     inputs.forEach((input) => {
       input.value = "";
     });
 
+    // close the modal
     closeModal(modal);
 
     values = [];
+
+    // add event listeners to the buttons to delete item
+    function deleteItem(id, item) {
+      event.preventDefault();
+      list.removeChild(item);
+      listItemsArray = listItemsArray.filter((el) => el[4] != id);
+      listItemsArray.forEach((el) => (el[0] = el[0] - 1));
+      let listItems = list.querySelectorAll(".list-item");
+      listItems.forEach((item) => {
+        item.querySelector(".list-item-content-text-title").innerHTML =
+          listItems.length;
+      });
+    }
+
+    // add event listeners to the buttons to edit the item
+    function editItem(id, item) {
+      openModal(modal);
+
+      let itemArr = listItemsArray.filter((item) => item[4] == id);
+
+      inputs[0].value = itemArr[0][1];
+      inputs[1].value = itemArr[0][2];
+
+      // select modal elements to edit
+      let saveEditButton = divElement.querySelector(".save");
+      let inputEditName = divElement.querySelectorAll("input")[0];
+      let inputEditURL = divElement.querySelectorAll("input")[1];
+
+      // when the modal is opened, first input field is focused
+      inputEditName.focus();
+
+      saveEditButton.addEventListener("click", (event) => {
+        item.querySelector(".list-item-content-text-description").innerHTML =
+          inputEditName.value;
+        item.querySelector(".list-item-content-text-title").innerHTML =
+          inputEditURL.value;
+      });
+      list.removeChild(item);
+      listItemsArray = listItemsArray.filter((el) => el[4] != id);
+      listItemsArray.forEach((el) => (el[0] = el[0] - 1));
+    }
+
+    console.log(listItemsArray);
   });
 
   return divElement;
